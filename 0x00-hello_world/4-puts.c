345d1a42f4b2f42c2a36319c1202868a1daa63a6
ranftjym <ranftmwangijym@gmail.com>
2023-02-17 03:39:24 +0000

    Print using puts in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - use puts function
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
