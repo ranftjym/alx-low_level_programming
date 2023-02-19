d117134168c8e78fb79a67c571435182832ed24b
ranftjym <ranftmwangijym@gmail.com>
2023-02-19 21:13:41 +0000

    Print using printf in C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - use printf function
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
