d26b08150304ff4012d069cc4341f34e3873b237
ranftjym <ranftmwangijym@gmail.com>
2023-02-22 03:00:38 +0000

    Uses _putchar to print character

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * _putchar - prints the character c to standard output
+ * @c: The character to print
+ *
+ * Return: 1 (Success)
+ * On error, -1 is returned, and errno is set
+*/
+int _putchar(char c)
+{
+	return 
+		9write(1, &c, 1));
+}
